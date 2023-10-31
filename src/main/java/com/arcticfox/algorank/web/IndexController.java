1babb7252689d3415df354a039507dac53ba90a0
ArcticFoox <jongsin16@naver.com>
2023-10-31 21:59:06 +0900

    프론트 부분 추가

--- src/main/java/com/arcticfox/algorank/web/IndexController.java
+++ src/main/java/com/arcticfox/algorank/web/IndexController.java
@@ -1,5 +1,33 @@
 package com.arcticfox.algorank.web;
 
+import com.arcticfox.algorank.service.ProblemService;
+import com.arcticfox.algorank.web.dto.ProblemResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+@RequiredArgsConstructor
+@Controller
 public class IndexController {
 
+    private final ProblemService problemService;
+
+    @GetMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @GetMapping("/problem/save")
+    public String postsSave() {
+        return "problem-save";
+    }
+
+    @GetMapping("/problem/update/{id}")
+    public String postsUpdate(@PathVariable Long id, Model model) {
+        ProblemResponseDto dto = problemService.findById(id);
+        model.addAttribute("problem", dto);
+        return "problem-update";
+    }
 }

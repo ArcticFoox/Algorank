b3fe76f6036708a7dd3248827e57984f33814a4e
ArcticFoox <jongsin16@naver.com>
2023-11-21 23:24:35 +0900

    구글 로그인 리펙토링

--- src/main/java/com/arcticfox/algorank/web/IndexController.java
+++ src/main/java/com/arcticfox/algorank/web/IndexController.java
@@ -1,17 +1,19 @@
 package com.arcticfox.algorank.web;
 
+import com.arcticfox.algorank.config.auth.LoginMember;
 import com.arcticfox.algorank.config.auth.dto.SessionMember;
 import com.arcticfox.algorank.service.MemberService;
 import com.arcticfox.algorank.service.ProblemService;
 import com.arcticfox.algorank.web.dto.ProblemListResponseDto;
 import com.arcticfox.algorank.web.dto.ProblemResponseDto;
-import jakarta.servlet.http.HttpSession;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @RequiredArgsConstructor
@@ -20,17 +22,18 @@ public class IndexController {
 
     private final ProblemService problemService;
     private final MemberService memberService;
-    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model){
+    public String index(Model model, @LoginMember SessionMember member){
         model.addAttribute("members", memberService.findAllByRank());
-        SessionMember member = (SessionMember) httpSession.getAttribute("member");
         if(member != null){
             model.addAttribute("memberName", member.getName());
         }
         return "index";
     }
+
+//    @GetMapping("/rank/day/{year}/{month}/{day}")
+//    public String dayRank(@PathVariable LocalDateTime year)
 
     @GetMapping("/member/{id}")
     public String memberProblems(@PathVariable Long id, Model model){
@@ -40,8 +43,7 @@ public class IndexController {
     }
 
     @GetMapping("/problem/save")
-    public String problemsSave(Model model) {
-        SessionMember member = (SessionMember) httpSession.getAttribute("member");
+    public String problemsSave(Model model, @LoginMember SessionMember member) {
         if(member != null){
             model.addAttribute("memberInfo", member);
         }
